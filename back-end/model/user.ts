f639b598a9a28947aff005b0b1cc92379512207a
Tomas Vaneynde <tvaneynde@users.noreply.github.com>
2024-11-01 15:00:35 +0100

    added user model

---
+++ back-end/model/user.ts
@@ -0,0 +1,39 @@
+export class User {
+    readonly username: string;
+    readonly email: string;
+    readonly password: string;
+    readonly role: string;
+
+    constructor(user: { email: string; username: string; password: string; role: string }) {
+        this.validate(user);
+        this.email = user.email;
+        this.username = user.username;
+        this.password = user.password;
+        this.role = user.role;
+    }
+
+    // Validate user input
+    validate(user: { email: string; username: string; password: string; role: string }) {
+        if (!user.email?.trim()) {
+            throw new Error('Email is required!');
+        }
+        if (!user.username?.trim()) {
+            throw new Error('username is required!');
+        }
+        if (!user.password?.trim()) {
+            throw new Error('Password is required!');
+        }
+        if (!user.role?.trim()) {
+            throw new Error('Role is required');
+        }
+    }
+
+    static from({ email, username, password, role }: User) {
+        return new User({
+            email,
+            username,
+            password,
+            role,
+        });
+    }
+}
