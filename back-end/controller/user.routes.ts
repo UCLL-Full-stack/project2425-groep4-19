5958ab352c86ec9c90e823ed5851721091f8befa
Tomas Vaneynde <tvaneynde@users.noreply.github.com>
2024-11-01 15:15:00 +0100

    added create user route

---
+++ back-end/controller/user.routes.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import { UserInput } from '../types';
+
+const userRouter = express.Router();
+
+//* Create new user
+userRouter.post('/', async (req: Request, res: Response) => {
+    try {
+        const user = <UserInput>req.body;
+        const result = await userService.createUser(user);
+        res.status(200).json(result);
+    } catch (error: any) {
+        res.status(400).json({
+            status: 'error',
+            errorMessage: error.message || 'An error occurred',
+        });
+    }
+});
