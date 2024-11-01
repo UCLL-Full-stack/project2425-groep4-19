ae46645e1594e7b7196eec2fb9aef2c96cabe99a
Tomas Vaneynde <tvaneynde@users.noreply.github.com>
2024-11-01 15:09:21 +0100

    configured app.ts correctly

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,20 +1,42 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-const app = express();
+const app: Express = express();
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
+//* Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/status', (req, res) => {
-    res.json({ message: 'Back-end is running...' });
-});
+//* JWT Secret
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+    console.error('JWT_SECRET is not defined. Please set it in your environment variables.');
+}
+
+//* Routes
+// app.use('/users', userRouter);
+
+//* Swagger
+const swaggerOpts = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Stockk API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['*src/controller/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOpts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port || 3000, () => {
     console.log(`Back-end is running on port ${port}.`);
