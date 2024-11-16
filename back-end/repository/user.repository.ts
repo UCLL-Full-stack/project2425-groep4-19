6485fe072cd4de364bb496a7126a8d749a9875cd
Tomas Vaneynde <tvaneynde@users.noreply.github.com>
2024-11-16 14:51:14 +0100

    updated user service and repository to use prisma

--- back-end/repository/user.repository.ts
+++ back-end/repository/user.repository.ts
@@ -1,18 +1,60 @@
 import { User } from '../model/user';
+import { UserInput } from '../types';
+import database from '../util/database';
+import bcrypt from 'bcrypt';
 
-const users: User[] = [];
+// TODO - Add comments to explain the functions
 
-const findByUsername = (username: string): User | undefined => {
+const findByUsername = async (username: string): Promise<User | undefined> => {
+    const users = await getAllUsers();
     return users.find((user) => user.username === username);
 };
 
-const getAllUsers = (): User[] => {
-    return users;
+const getAllUsers = async (): Promise<User[]> => {
+    try {
+        const users = await database.user.findMany();
+        return users.map((user) => User.from(user));
+    } catch (error) {
+        throw new Error(`Error getting users: ${error}`);
+    }
 };
 
-const createUser = (user: User): User => {
-    users.push(user);
-    return user;
+const hashPassword = async (password: string): Promise<string> => {
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+};
+
+const createUser = async ({
+    email,
+    username,
+    password,
+    role,
+}: UserInput): Promise<{ message: string; user: User }> => {
+    try {
+        // check if user already exists
+        const users = await getAllUsers();
+        if (users.map((user) => user.email).includes(email)) {
+            throw new Error('User already exists');
+        }
+
+        // Hash password and create user
+        const hashedPassword = await hashPassword(password);
+        const newUser = new User({ email, username, password: hashedPassword, role });
+
+        // Save user to database
+        const user = await database.user.create({
+            data: {
+                email: newUser.email,
+                username: newUser.username,
+                password: newUser.password,
+                role: newUser.role,
+            },
+        });
+
+        return { message: 'User registered successfully', user: newUser };
+    } catch (error) {
+        throw new Error(`Error creating user: ${error}`);
+    }
 };
 
 export default {
