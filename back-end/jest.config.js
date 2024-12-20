1f9d963b402e9b63e1fa22d75d73cd0a3e22054f
AndyAlijiUCLL <r0991778@ucll.be>
2024-12-20 19:57:52 +0100

    update testing configuration and add LoginForm tests

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+      '\\.[jt]sx?$': 'esbuild-jest',
+  },
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
